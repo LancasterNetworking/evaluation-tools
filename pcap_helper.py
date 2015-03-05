9e5035251cef215b791b7b8e93dcf1be6c1a7067
richard <rick_withnell@hotmail.com>
2015-03-05 16:02:49 +0000

    added pcap_helper

---
+++ pcap_helper.py
@@ -0,0 +1,50 @@
+import scapy
+import scapy.layers.l2
+import scapy.layers.inet
+from scapy.utils import PcapReader
+from scapy.all import *
+
+
+def print_psuedo_header(p):
+    if UDP in p:
+        print ''.join(["UDP: (SRC:", p[IP].src,':', str(p[UDP].sport),
+                        ' DST:', p[IP].dst ,':', str(p[UDP].dport),')',
+                        ' LEN: ', str(p[UDP].len)])
+    elif TCP in p:
+        print ''.join(["TCP: (", p[IP].src,':', str(p[TCP].sport),
+                        ' DST:', p[IP].dst, ':', str(p[TCP].dport),')',
+                        ' LEN: ', str(p[UDP].len)])
+
+def psuedo_header(p):
+    if any((UDP, TCP)) in p:
+        return (p[IP].src, p[IP].dst, p[IP].sport, p[IP].dport)
+
+
+def parse_tcp_flows(packets):
+    flows = {}
+    for p in packets:
+        if psuedo_header(p) not in flows:
+            flows[psuedo_header(p)] = []
+        flows[psuedo_header(p)].append(p)
+    return flows
+
+
+def load_udp_packets(location):
+    return load_packets(location, filter=lambda x: True if UDP in x else False)
+
+
+def load_tcp_packets(location):
+    return load_packets(location, filter=lambda x: True if TCP in x else False)
+
+
+def load_packets(location, filter=lambda x: x is not None):
+    pkts = []
+    packets = rdpcap(location)
+    for p in packets:
+        if filter(p):
+            pkts.append(p)
+    return pkts
+
+
+def get_total_size(packets):
+    return sum(p.length for p in packets)
